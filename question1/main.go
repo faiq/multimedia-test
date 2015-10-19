7b2eb65b93ee36d459b700b663fc5c88c805d0fb
faiq <faiqrazarizvi@gmail.com>
2015-10-18 23:45:55 -0400

    starts basic test

--- question1/main.go
+++ question1/main.go
@@ -46,20 +46,19 @@ func readLines(filename string) ([]string, error) {
 	return lines, nil
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("looks like you didn't insert a text file to analyze")
-		return
+func sortState(lineLen int, state map[string]int) PairList {
+	pl := make(PairList, lineLen)
+	i := 0
+
+	for k, v := range state {
+		pl[i] = Pair{k, v}
+		i++
 	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
 
-	fileToRead := os.Args[1]
-	lines, err := readLines(fileToRead)
-
-	if err != nil {
-		fmt.Printf("looks like there was an error reading the file")
-		return
-	}
-
+func ProcessLines(lines []string) map[string]int {
 	state := make(map[string]int) //state will keep a mapping of sanitized names
 
 	for _, line := range lines {
@@ -74,17 +73,26 @@ func main() {
 			state[line] = 1
 		}
 	}
+	return state
+}
 
-	pl := make(PairList, len(lines))
-	i := 0
-
-	for k, v := range state {
-		pl[i] = Pair{k, v}
-		i++
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("looks like you didn't insert a text file to analyze")
+		return
 	}
-	sort.Sort(sort.Reverse(pl))
 
-	for _, pair := range pl {
+	fileToRead := os.Args[1]
+	lines, err := readLines(fileToRead)
+
+	if err != nil {
+		fmt.Printf("looks like there was an error reading the file")
+		return
+	}
+
+	state := ProcessLines(lines)
+	sortedMap := sortState(len(lines), state)
+	for _, pair := range sortedMap {
 		if pair.Value > 1 {
 			fmt.Printf("%s %d\n", pair.Key, pair.Value)
 		}
